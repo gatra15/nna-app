961970fda6163ca1e4c212156d37d125b41b6078
gatra15 <gatra881@gmail.com>
2025-04-30 13:20:45 +0700

    add config server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devServer: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
   compatibilityDate: "2025-04-26",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
